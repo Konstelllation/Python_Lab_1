2b933531c769acba0d5ca8fe37a36e74ada1b1d1
VitaliyPitsa <harchenkobo2003@gmail.com>
2022-10-09 17:57:28 +0300

    Переделал индивидуальное задание

--- Lab_1_Individual.py
+++ Lab_1_Individual.py
@@ -1,25 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+
 if __name__ == '__main__':
-    if __name__ == '__main__':
-        list_of_dicts = list()
+    list_of_dicts = list()
 
-        n = int(input("Введите количество студентов: "))
-        for i in range(n):
-            d = dict()
+    n = int(input("Введите количество студентов: "))
+    for i in range(n):
+        d = dict()
 
-            d['Фамилия и инициалы'] = input("Введите ФИО: ")
-            d['Номер группы'] = int(input("Введите номер группы: "))
-            d['Успеваемость'] = list(map(int, input("Введите оценки: ").split()))
+        d['Фамилия и инициалы'] = input("Введите ФИО: ")
+        d['Номер группы'] = int(input("Введите номер группы: "))
+        d['Успеваемость'] = input("Введите оценки: ")
 
-            list_of_dicts.append(d)
+        list_of_dicts.append(d)
 
-        list_of_dicts = sorted(list_of_dicts, key=lambda x: x["Фамилия и инициалы"])
+    list_of_dicts = sorted(list_of_dicts, key=lambda x: x["Фамилия и инициалы"])
 
-        is_find = False
+    line = '+-{}-+-{}-+-{}-+-{}-+'.format(
+        '-' * 4,
+        '-' * 30,
+        '-' * 20,
+        '-' * 20
+    )
+    print(line)
+    print(
+        '| {:^4} | {:^30} | {:^20} | {:^20} |'.format(
+            "No",
+            "Фамилия и инициалы",
+            "Номер группы",
+            "Успеваемость"
+        )
+    )
+    print(line)
 
-        for item in list_of_dicts:
-            if 2 in item['Успеваемость']:
-                print(f'Двоишники:\n {item["Фамилия и инициалы"]}\nГруппа: {item["Номер группы"]}\n\n')
-                is_find = True
+    for idx, d in enumerate(list_of_dicts, 1):
+        print(
+            '| {:>4} | {:<30} | {:<20} | {:>20} |'.format(
+                idx,
+                d.get('Фамилия и инициалы', ''),
+                d.get('Номер группы', ''),
+                d.get('Успеваемость', '')
+            )
+        )
 
-        if not is_find:
-            print("Нет двоишников!")
+    print(line)
+
+    is_find = False
+
+    print("\nДвоишники:\n")
+    for item in list_of_dicts:
+        if 2 in list(map(int, item['Успеваемость'].split())):
+            print(f'{item["Фамилия и инициалы"]}\nГруппа: {item["Номер группы"]}\n\n')
+            is_find = True
+
+    if not is_find:
+        print("Нет двоишников!")
